5e0ab6457be4c88f2781d6bedacda0c2d9628770
niklasbentzen <niklas@bentzen.it>
2026-01-15 16:59:30 +0100

    restart

--- src/types/leaflet-gpx.d.ts
+++
@@ -1,8 +0,0 @@
-import * as L from "leaflet";
-
-declare module "leaflet" {
-  // Define a minimal type for L.GPX so TS doesn't complain
-  class GPX extends L.FeatureGroup {
-    constructor(url: string, options?: any);
-  }
-}

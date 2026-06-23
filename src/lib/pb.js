4beba4458d5d067f29c50b7f88fecbf9c8baf530
niklasbentzen <niklas@bentzen.it>
2026-06-23 16:07:41 +0200

    local files fix

--- src/lib/pb.js
+++ src/lib/pb.js
@@ -1,6 +1,8 @@
 import PocketBase from "pocketbase";
 
-export const pb = new PocketBase(import.meta.env.VITE_PB_URL);
+export const pb = new PocketBase(
+  import.meta.env.VITE_PB_URL || window.location.origin,
+);
 
 // Prevent React dev-mode double renders from cancelling requests
 pb.autoCancellation(false);
